2de02c167bd4e76fc4bd7f135c457370575d017a
Elie <niyubwayoiraelie5777@gmail.com>
2025-04-18 23:58:17 +0200

    Implement Pagination & Filtering final Part

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import ProductResult from "@/components/search/ProductResult";
 import SearchFilters from "@/components/search/SearchFilters";
 import {getProducts} from "@/lib/actions/products";
+import Pagination from "@/components/search/pagination";
 
 export default async function Component({searchParams}: {searchParams: { name: string, category: string, minPrice: string, page: string}}) {
 
@@ -15,6 +16,7 @@ export default async function Component({searchParams}: {searchParams: { name: s
     <div className="grid md:grid-cols-[300px_1fr] gap-8 px-4 md:px-8 py-20">
       <div className="bg-white rounded-lg shadow-sm dark:bg-gray-950 p-6 space-y-6">
         <SearchFilters />
+          <Pagination />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products?.map((product) => (
